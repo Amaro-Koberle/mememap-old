f06cb3571d67a704f3a4e9430e3517264fdb8806
Amaro-Koberle <amaro@amarokoberle.com>
2022-03-01 22:57:46 +0100

    set up prisma and database

--- app/routes/nodes/$nodeId.tsx
+++ app/routes/nodes/$nodeId.tsx
@@ -1,7 +1,26 @@
+import type { LoaderFunction } from 'remix';
+import { useLoaderData } from 'remix';
+import type { Node } from '@prisma/client';
+
+import { db } from '~/utils/db.server';
+
+type LoaderData = { node: Node };
+
+export const loader: LoaderFunction = async ({ params }) => {
+	const node = await db.node.findUnique({
+		where: { id: params.nodeId },
+	});
+	if (!node) throw new Error('Node not found');
+	const data: LoaderData = { node };
+	return data;
+};
+
 export default function NodeRoute() {
+	const data = useLoaderData<LoaderData>();
 	return (
-		<div>
-			<p>Node Route</p>
-		</div>
+		<>
+			<h1>{data.node.name}</h1>
+			<p>{data.node.content}</p>
+		</>
 	);
 }
